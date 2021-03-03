de115b9efc2530b45970423a29b8e5c7a9fff7f4
praveen <praveenbala1211@gmail.com>
2021-03-03 15:52:13 +0530

    admin panel login

--- profile_api/models.py
+++ profile_api/models.py
@@ -35,7 +35,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     email=models.EmailField(max_length=225,unique=True)
     name=models.CharField(max_length=255)
     is_active=models.BooleanField(default=True)
-
+    is_staff=models.BooleanField(default=False)
 
     object=UserProfileManager()
 
